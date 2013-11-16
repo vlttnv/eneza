3d422e44fb40b7ecf208540315426205b55bbce8
Caroline Player <player_c@hotmail.co.uk>
2013-11-16 23:00:58 +0000

    student text box for assign test

--- studentxJS.js
+++ studentxJS.js
@@ -1,3 +1,24 @@
 function assignTest()	{
+
+	
+
+	var container = document.getElementById("container")
+	var textBoxContainer = document.getElementById("textBoxContainer")
+
 	var textBox = document.createElement('div')
+	textBox.innerHTML='<form>Test: <input type="text" name="testName"><br></form>'
+
+	textBoxContainer.appendChild(textBox)
+
+	var cancelBtn = document.createElement('button')
+	textBoxContainer.appendChild(cancelBtn)
+	
+
+	var text = document.createTextNode("Cancel")
+
+	cancelBtn.onclick = function()	{
+		textBoxContainer.removeChild(textBox)
+		textBoxContainer.removeChild(cancelBtn)
+	};
+	cancelBtn.appendChild(text);
 }
