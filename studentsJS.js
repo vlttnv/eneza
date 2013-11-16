9df5d9d1a0c2151aa45e5e1ea22e15a1f7a96264
Caroline Player <player_c@hotmail.co.uk>
2013-11-16 18:31:58 +0000

    skeletons

---
+++ studentsJS.js
@@ -0,0 +1,7 @@
+function studentsJS(){
+	
+	var newStudentDiv = document.createElement('div')
+	newStudentDiv.innerHTML = "<p>John Doe</p>"
+	document.getElementById("studenthere").appendChild(newStudentDiv)
+	//document.write(myyear+"/"+mymonth+"/"+mytoday)
+}
