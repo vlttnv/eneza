4eaeb4152ca9ff606b771932e7d9bb7bdcb58e48
Caroline Player <player_c@hotmail.co.uk>
2013-11-16 20:25:03 +0000

    Adding students in JS hardcoded
    Can add students with a method in JS. Need to wait for data to grab
    info completely on the fly

--- studentsJS.js
+++ studentsJS.js
@@ -1,10 +1,19 @@
 function studentsJS(){
 	
+	newStudent("John Doe", "Male", "Foo", "Bar")
+	newStudent("Jane Doe", "Female", "Foo", "Bar")
+
+}
+
+function newStudent(name, gender, school, region)	{
+
 	var newStudentDiv = document.createElement('div')
-	newStudentDiv.innerHTML = "<p>John Doe</p>"
-	document.getElementById("studenthere").appendChild(newStudentDiv)
+	newStudentDiv.className="row"
+	innerhtml = '<div class="col-xs-3"><p>' + name + '</p>' +
+      '</div><div class="col-xs-3"><p>' + gender + '</p>' + 
+      '</div><div class="col-xs-3"><p>' + school + '</p>' +
+      '</div><div class="col-xs-3"><p>' + region + '</p></div>'
 
-	
-
-	//alert("Page is loaded")
+	newStudentDiv.innerHTML = innerhtml
+	document.getElementById("studentrows").appendChild(newStudentDiv)
 }
