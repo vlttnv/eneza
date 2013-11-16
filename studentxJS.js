87ec9ed6448e47a3b2b1e591060fd151c4b8be25
Caroline Player <player_c@hotmail.co.uk>
2013-11-16 21:10:40 +0000

    Sending data to student page
    In the list of students, can select a student and it sends their id
    which can be used to collect their information

--- studentxJS.js
+++ studentxJS.js
@@ -9,13 +9,11 @@ function newStudent(id, name, gender, school, region)	{
 
 	var newStudentDiv = document.createElement('div')
 	newStudentDiv.className="row"
-	innerhtml = '<a href="./student.html?userid=' + id + '">' +
-	  '<div class="col-xs-1"><p>' + id + '</p>' +
+	innerhtml = '<div class="col-xs-1"><p>' + id + '</p>' +
       '</div><div class="col-xs-3"><p>' + name + '</p>' +
       '</div><div class="col-xs-2"><p>' + gender + '</p>' + 
       '</div><div class="col-xs-3"><p>' + school + '</p>' +
-      '</div><div class="col-xs-3"><p>' + region + '</p></div>' +
-      '</form>'
+      '</div><div class="col-xs-3"><p>' + region + '</p></div>'
 
 	newStudentDiv.innerHTML = innerhtml
 
